fcb8c2c8b4a8d6afdcb224fcbc1596d2625cc16d
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-08 18:03:13 +0100

    PasosDeDocumentacionEnLaRamaMaster

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Problemas con la generación el Javadoc.
  */
 package calculadora;
 
@@ -51,4 +49,7 @@ public class Calculadora {
         this.operador = operacion;
     }
     
+    public static void main(String[] args) {
+        System.out.println("");
+    }
 }
