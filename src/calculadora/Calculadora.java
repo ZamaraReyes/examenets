867d45d59a3122e36f632be3b386539f279d4ece
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-08 16:59:22 +0100

    commit quarto

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -12,48 +12,32 @@ public class Calculadora {
     private String operador2;
     private String operacion;
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
-    /**
-     * @return the operador1
-     */
+    
     public String getOperador1() {
         return operador1;
     }
 
-    /**
-     * @return the operador2
-     */
+
     public String getOperador2() {
         return operador2;
     }
 
-    /**
-     * @return the operacion
-     */
+
     public String getOperacion() {
         return operacion;
     }
 
-    /**
-     * @param operador1 the operador1 to set
-     */
+
     public void setOperador1(String operador1) {
         this.operador1 = operador1;
     }
 
-    /**
-     * @param operador2 the operador2 to set
-     */
+
     public void setOperador2(String operador2) {
         this.operador2 = operador2;
     }
 
-    /**
-     * @param operacion the operacion to set
-     */
+
     public void setOperacion(String operacion) {
         this.operacion = operacion;
     }
