fc0fe6656bbe99da669f3280e6b48bce75dc3925
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-08 17:06:36 +0100

    CambioDeNombreDeOperador2PorOper2

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -9,7 +9,7 @@ package calculadora;
 public class Calculadora {
     
     private String operador1;
-    private String operador2;
+    private String oper2;
     private String operador;
 
     
@@ -19,7 +19,7 @@ public class Calculadora {
 
 
     public String getOperador2() {
-        return operador2;
+        return oper2;
     }
 
 
@@ -34,7 +34,7 @@ public class Calculadora {
 
 
     public void setOperador2(String operador2) {
-        this.operador2 = operador2;
+        this.oper2 = operador2;
     }
 
 
