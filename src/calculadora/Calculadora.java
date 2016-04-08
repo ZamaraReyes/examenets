b58b508d4fb5002682e9d9e2efed1306babf262f
Zare Red <ZareRed@MacBook-Pro-de-Laura.local>
2016-04-08 17:56:10 +0100

    PasosDeDocumentacionEnLaRamaMaster

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -8,35 +8,44 @@ package calculadora;
 
 public class Calculadora {
     
+    
     private String operador1;
-    private String operador2;
+    private String oper2;
     private String operador;
 
+    
+    /**
+     * Atributo operador1.
+     */
     
     public String getOperador1() {
         return operador1;
     }
 
-
-    public String getOperador2() {
-        return operador2;
-    }
-
-
-    public String getOperacion() {
-        return operador;
-    }
-
-
     public void setOperador1(String operador1) {
         this.operador1 = operador1;
     }
 
-
+    /**
+     * Atributo oper2.
+     */
+    
+    public String getOperador2() {
+        return oper2;
+    }
+    
     public void setOperador2(String operador2) {
-        this.operador2 = operador2;
+        this.oper2 = operador2;
     }
 
+    
+    /**
+     * Atributo operador.
+     */
+    
+    public String getOperacion() {
+        return operador;
+    }
 
     public void setOperacion(String operacion) {
         this.operador = operacion;
